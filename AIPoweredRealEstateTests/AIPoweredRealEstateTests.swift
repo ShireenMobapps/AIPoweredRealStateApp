cd53ca9a4ef70bf69f2ee322ef7071c59c5f678c
ShireenMobapps <mobappssolutions187@gmail.com>
2026-08-18 11:15:12 +0530

    Initial Commit

---
+++ AIPoweredRealEstateTests/AIPoweredRealEstateTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIPoweredRealEstateTests.swift
+//  AIPoweredRealEstateTests
+//
+//  Created by Shireen on 18/08/26.
+//
+
+import Testing
+@testable import AIPoweredRealEstate
+
+struct AIPoweredRealEstateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
